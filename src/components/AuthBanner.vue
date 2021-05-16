c1139eca889490588570c1d776a10a3a89a7bd97
amir <amirheydari9@gmail.com>
2021-05-16 15:02:14 -0700

    assessment service

--- src/components/AuthBanner.vue
+++ src/components/AuthBanner.vue
@@ -1,18 +1,12 @@
 <template>
-    <v-col cols="12" :md="md" class="gra-bg d-flex flex-column align-center justify-center">
+    <v-col cols="4" :md="md" class="gra-bg">
       <v-img
           src="/big-satra.jpg"
-          width="100%"
-          height="100%"
-          :max-width="$vuetify.breakpoint.mdAndUp ? '100%' : '300px'"
+          width="600px"
+          height="600px"
+          class="text-center"
+          style="margin:50px auto"
       ></v-img>
-      <!--    <span class="white&#45;&#45;text font-weight-bold"-->
-      <!--          :class="$vuetify.breakpoint.mdAndUp ? 'mt-10 display-1' : 'title'">-->
-      <!--به سامانه خوش آمدید-->
-      <!--        </span>-->
-      <!--    <span class="mt-3 font-weight-bold hidden-sm-and-down text-center">-->
-      <!--همین حالا حساب کاربری خودت را بساز و دوران جدید وبلاگ نویسی را شروع کن.-->
-      <!--        </span>-->
     </v-col>
 </template>
 
